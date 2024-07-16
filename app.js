07af90f86b763fd2c32bd6769e1afe3805726a81
alexwithstars <rayo080378@gmail.com>
2024-07-15 22:39:22 -0600

    init test

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import { PORT } from './constants.js'
+
+const app = express()
+app.disable('x-powered-by')
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
